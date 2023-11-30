8fd57e799dd46dca9c384c982c2847a31fd773db
mohamedkhedr700 <M.khedr@rowaad.net>
2023-11-30 21:55:16 +0200

    Add with dispatch trait

--- src/Traits/Dispatcher/WithType.php
+++ src/Traits/Dispatcher/WithType.php
@@ -5,11 +5,6 @@ namespace Raid\Core\Dispatcher\Traits\Dispatcher;
 trait WithType
 {
     /**
-     * Type of dispatcher
-     */
-    public const TYPE = '';
-
-    /**
      * {@inheritdoc}
      */
     public static function type(): string
